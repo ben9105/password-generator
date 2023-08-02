47e841f514fca834b5caee077a1599a418378b14
Ben <benjaminshew@gmail.com>
2023-08-02 00:49:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+const passwordField = document.getElementById('password-field')
+const clipboardButton = document.getElementById('clipboard-btn')
+
+const clearPassword = () => {
+    passwordField.innerHTML = ''
+}
+
+function password() {
+    if (passwordField.value = '') {
+        for (let i = 0; i < 15; i++) {
+            let randomChar = (Math.floor(Math.random() * characters.length))
+            passwordField.innerHTML += characters[randomChar]
+        }
+    } else {
+        clearPassword()
+        for (let i = 0; i < 15; i++) {
+            let randomChar = (Math.floor(Math.random() * characters.length))
+            passwordField.innerHTML += characters[randomChar]
+        }
+    }
+}
+
+function clipboard() {
+
+}
